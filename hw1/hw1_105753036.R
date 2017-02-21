e68a74385b752105abfb1dc08a567233b91fdf09
yad50968 <yad50968@gmail.com>
2017-02-21 18:48:50 +0800

    add round function

--- hw1/hw1_105753036.R
+++ hw1/hw1_105753036.R
@@ -1,10 +1,13 @@
 args<-commandArgs(TRUE)
+inputNameWithOutExtension <- sub('\\.csv$', '', args[1])
+outputName <- args[2]
+inputName <- args[1]
 
-data <-read.table(args[1], header = TRUE, sep = ",", colClasses = c("NULL",NA,NA,"NULL"))
-inputName <- sub('\\.csv$', '', args[1])
+data <-read.table(inputName, header = TRUE, sep = ",", colClasses = c("NULL",NA,NA,"NULL"))
 
 colMax <- function(data) apply(data, 1, max, na.rm = TRUE)
-maxW <- colMax(t(data$weight))
-maxH <- colMax(t(data$height))
-outputData = data.frame(set = inputName, weight = maxW, height = maxH)
-write.csv(outputData, file = args[2], row.names = FALSE)
+maxW <- round(colMax(t(data$weight)), 2)
+maxH <- round(colMax(t(data$height)), 2)
+
+outputData = data.frame(set = inputNameWithOutExtension, weight = maxW, height = maxH)
+write.csv(outputData, file = outputName, row.names = FALSE)
