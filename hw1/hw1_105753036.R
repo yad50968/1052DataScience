409e9638ded15ffd0e6cb36435c00b0d653549fe
Liu.Z.Y <yad50968@gmail.com>
2017-02-21 21:54:15 +0800

    Update hw1_105753036.R

--- hw1/hw1_105753036.R
+++ hw1/hw1_105753036.R
@@ -1,13 +1,19 @@
-args<-commandArgs(TRUE)
-inputNameWithOutExtension <- sub('\\.csv$', '', args[1])
-outputName <- args[2]
+args<-commandArgs(TRUE)  # get the args
 inputName <- args[1]
+inputNameWithOutExtension <- sub('\\.csv$', '', inputName)  # remove filename extension
+outputName <- args[2]
 
-data <-read.table(inputName, header = TRUE, sep = ",", colClasses = c("NULL",NA,NA,"NULL"))
+
+data <-read.table(inputName, header = TRUE, sep = ",", colClasses = c("NULL",NA,NA,"NULL")) 
+# read input data only for height and weight
 
 colMax <- function(data) apply(data, 1, max, na.rm = TRUE)
+# function for get the max value for column
+  
 maxW <- round(colMax(t(data$weight)), 2)
 maxH <- round(colMax(t(data$height)), 2)
+# let factor only two bits
 
 outputData = data.frame(set = inputNameWithOutExtension, weight = maxW, height = maxH)
 write.csv(outputData, file = outputName, row.names = FALSE)
+# output data
