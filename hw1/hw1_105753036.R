048f4bd37781d2fc5644208a453b6c877b8b415d
liu.z.y <yad50968@gmail.com>
2017-02-21 22:08:01 +0800

    fix bug

--- hw1/hw1_105753036.R
+++ hw1/hw1_105753036.R
@@ -1,7 +1,21 @@
 args<-commandArgs(TRUE)  # get the args
-inputName <- args[1]
+if (length(args) == 0) {
+    stop("USAGE: Rscript hw1_105753036.R -files {input-file-name} -out {output-file-name")
+}
+i <- 1
+while(i < length(args)) {
+    if(args[i] == "-files") {
+        inputName <- args[i+1]
+        i <- i + 1
+    } else if (args[i] == "-out") {
+        outputName <- args[i+1]
+        i <- i + 1
+    } else {
+        stop(paste("Unknown flag", args[i]), call.=FALSE)
+    }
+    i <- i + 1
+}
 inputNameWithOutExtension <- sub('\\.csv$', '', inputName)  # remove filename extension
-outputName <- args[2]
 
 
 data <-read.table(inputName, header = TRUE, sep = ",", colClasses = c("NULL",NA,NA,"NULL")) 
